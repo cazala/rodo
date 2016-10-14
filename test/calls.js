e731c81f7c9d36ab69c6150cb34206b9843fb291
Nicolas Escalante <nlante@gmail.com>
2016-10-14 12:07:17 -0300

    add missed calls tests

---
+++ test/calls.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const rodo = require('../src');
+const request = require('supertest-as-promised');
+
+describe('calls', () => {
+  let mock;
+
+  beforeEach(() => (
+    mock = rodo()
+  ));
+
+  afterEach(() => (
+    mock.clean()
+  ));
+
+  describe('not called', () => {
+    let myCall;
+
+    beforeEach(() => (
+      myCall = mock
+        .get('/foo')
+        .reply()
+    ));
+
+    it('should not be called', () => {
+      myCall.invoked.should.eql(false);
+      myCall.invokedOnce.should.eql(false);
+      myCall.invokedTwice.should.eql(false);
+      myCall.invokedThrice.should.eql(false);
+      myCall.invokedCount.should.eql(0);
+    });
+  });
+
+  describe('called once', () => {
+    let myCall;
+
+    beforeEach(() => (
+      myCall = mock
+        .get('/foo')
+        .reply()
+    ));
+
+    beforeEach(() => (
+      request(mock)
+        .get('/foo')
+    ));
+
+    it('should be called once', () => {
+      myCall.invoked.should.eql(true);
+      myCall.invokedOnce.should.eql(true);
+      myCall.invokedTwice.should.eql(false);
+      myCall.invokedThrice.should.eql(false);
+      myCall.invokedCount.should.eql(1);
+    });
+  });
+
+  describe('called twice', () => {
+    let myCall;
+
+    beforeEach(() => (
+      myCall = mock
+        .get('/foo')
+        .reply()
+    ));
+
+    beforeEach(() => (
+      Promise.all([
+        request(mock).get('/foo'),
+        request(mock).get('/foo'),
+      ])
+    ));
+
+    it('should be called twice', () => {
+      myCall.invoked.should.eql(true);
+      myCall.invokedOnce.should.eql(false);
+      myCall.invokedTwice.should.eql(true);
+      myCall.invokedThrice.should.eql(false);
+      myCall.invokedCount.should.eql(2);
+    });
+  });
+
+  describe('called thrice', () => {
+    let myCall;
+
+    beforeEach(() => (
+      myCall = mock
+        .get('/foo')
+        .reply()
+    ));
+
+    beforeEach(() => (
+      Promise.all([
+        request(mock).get('/foo'),
+        request(mock).get('/foo'),
+        request(mock).get('/foo'),
+      ])
+    ));
+
+    it('should be called twice', () => {
+      myCall.invoked.should.eql(true);
+      myCall.invokedOnce.should.eql(false);
+      myCall.invokedTwice.should.eql(false);
+      myCall.invokedThrice.should.eql(true);
+      myCall.invokedCount.should.eql(3);
+    });
+  });
+});
