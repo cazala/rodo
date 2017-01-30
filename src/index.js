96f88a7ea7107f2cadaf64bf6415e0c6b0392893
Nicolas Escalante <nlante@gmail.com>
2017-01-30 10:06:22 -0300

    add middleware option

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const Builder = require('./builder');
 const http = require('http');
 
 function rodo(port, hostname) {
+  const middlewares = [];
   const server = http.createServer((req, res) => {
     const rule = server.rules.find(a => a.match(req));
+
+    middlewares.forEach(m => m(req, res));
 
     if (rule) {
       server.calls.push(rule);
@@ -18,10 +21,15 @@ function rodo(port, hostname) {
         return;
       }
     }
+
     // eslint-disable-next-line no-param-reassign
     res.statusCode = 404;
     res.end();
   });
+
+  server.use = function use(middleware) {
+    middlewares.push(middleware);
+  };
 
   ['get', 'post', 'put', 'delete', 'patch'].forEach((method) => {
     server[method] = (path) => {
---
+++ test/middleware.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const rodo = require('../src');
+const request = require('supertest-as-promised');
+
+describe('middleware', () => {
+  let mock;
+
+  beforeEach(() => (
+    mock = rodo()
+  ));
+
+  afterEach(() => (
+    mock.clean()
+  ));
+
+  describe('adding a middleware', () => {
+    let myReq;
+
+    beforeEach(() => {
+      mock
+        .use((req) => {
+          myReq = req;
+        });
+    });
+
+    it('should call the middleware a json', () => (
+      request(mock)
+        .get('/foo')
+        .expect(() => {
+          myReq.url.should.eql('/foo');
+        })
+    ));
+  });
+});
